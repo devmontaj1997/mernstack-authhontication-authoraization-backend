d5eaca483c1faf035b4278f674128ac77bfb9608
devmontaj1997 <sekhmontajulali1997@gmail.com>
2024-10-14 17:20:18 +0530

    connect clint

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:4173"],
+    origin: ["http://localhost:3000", "http://localhost:4173", "https://mernstack-authhontication-authoraization-fontend.vercel.app"],
     credentials: true,
   })
 );
